2d2495d902b8bf82c00a1933b3eaf266bd7db15b
Israel Dryer <42584742+israel-dryer@users.noreply.github.com>
2019-10-31 13:01:31 -0400

    compress code, add stats, bug fix

--- code_counter.py
+++ code_counter.py
@@ -2,11 +2,12 @@
     Count Counter
     A program that counts the lines of code and code characters in a code-base
     Author      :   Israel Dryer
-    Modified    :   2019-10-30
+    Modified    :   2019-10-31
 
 """
 import PySimpleGUI as sg
 from os import system
+import statistics as stats
 
 
 def read_file(filename):
@@ -17,61 +18,56 @@ def read_file(filename):
     # clean and parse initial raw data
     data = [row.strip() for row in raw if row.strip()]
 
-    # identify and strip hash comments
+    # remove hash comments
     stage1 = []
     for row in data:
-        ix = row.find('#')
-        if ix == -1:
+        if row.find('#') != -1:
+            stage1.append(row[:row.find('#')])
+        else:
             stage1.append(row)
-        else:
-            if row[:ix]:
-                stage1.append(row[:ix])
 
-    # identify and strip 3x" multi-line comments
+    # remove " multiline comments
     stage2 = []
-    ml_flag = False
+    ml_flag = False # multiline comment flag
     for row in stage1:
-        ml_count = row.count(r'"""')
-        if ml_count == 0 and not ml_flag:
+        if row.count(r'"""') == 0 and not ml_flag: # not a comment line
             stage2.append(row)
-        elif ml_count == 1 and ml_flag:
-            ml_flag = False
-            ix = row.find(r'"""')
-            stage2.append(row[ix+1:])
-        elif ml_count == 1 and not ml_flag:
+        elif row.count(r'"""') == 1 and not ml_flag: # starting comment line
             ml_flag = True
-            ix = row.find(r'"""')
-            stage2.append(row[:ix])
+            stage2.append(row[:row.find('"""')])
+        elif row.count(r'"""') == 1 and ml_flag: # ending comment line
+            ml_flag = False
+            stage2.append(row[row.find('"""')+1:])
         else:
             continue
 
-    # identify and strip 3x' multi-line comments
+    # remove ' multiline comments
     stage3 = []
-    ml_flag = False
+    ml_flag = False # multiline comment flag
     for row in stage2:
-        ml_count = row.count(r"'''")
-        if ml_count == 0 and not ml_flag:
+        if row.count(r"'''") == 0 and not ml_flag: # not a comment line
             stage3.append(row)
-        elif ml_count == 1 and ml_flag:
-            ml_flag = False
-            ix = row.find(r"'''")
-            stage3.append(row[ix+1:])
-        elif ml_count == 1 and not ml_flag:
+        elif row.count(r"'''") == 1 and not ml_flag: # starting comment line
             ml_flag = True
-            ix = row.find(r"'''")
-            stage3.append(row[:ix])
+            stage3.append(row[:row.find("'''")])
+        elif row.count(r"'''") == 1 and ml_flag: # ending comment line
+            ml_flag = False
+            stage3.append(row[row.find("'''")+1:])
         else:
             continue
 
-    final = [row for row in stage3 if row not in ('', "''", '""')]
+    clean_code = [row for row in stage3 if row not in ('', "''", '""')]
 
-    # row and character rounds
-    char_count = []
-    for row in stage3:
-        char_count.append(len(row))
-    line_count = len(stage3)
+    # row and character rounds / for calc stats, histogram, charts
+    char_cnt = [len(row) for row in clean_code] 
 
-    return final, char_count, line_count
+    # statistics
+    code_stats = {
+        'lines': len(clean_code), 'char_per_line': sum(char_cnt)//len(clean_code), 
+        'count': sum(char_cnt), 'mean': stats.mean(char_cnt), 'median': stats.median(char_cnt), 
+        'pstdev': stats.pstdev(char_cnt), 'min': min(char_cnt), 'max': max(char_cnt)}
+
+    return clean_code, char_cnt, code_stats
 
 
 def main():
@@ -81,29 +77,23 @@ def main():
         sg.popup_error('No file selected', title='ERROR!')
         return
     else:
-        clean_data, char_count, line_count = read_file(filename)
+        clean_code, char_cnt, code_stats = read_file(filename)
 
         with open('output.txt', 'w') as f:
             # write statistics to file
-            f.write('{}statistics'.format(' '*7))
-            f.write('\n{}\n'.format('*'*25))
-            f.write('Count of lines: {}\n'.format(line_count))
-            f.write('Count of characters: {}\n'.format(sum(char_count)))
-            f.write('Avg characters per line: {}\n\n'.format(sum(char_count)//line_count))
-            f.write('{}clean data'.format(' '*7))
-            f.write('\n{}\n'.format('*'*25))
-
+            f.write('STATISTICS\n' + '='*25 + '\n')
+            for key, value in code_stats.items():
+                f.write('{}: {:,.0f}\n'.format(key, value))
+            
             # write clean data to file
-            for row in clean_data:
+            f.write('\n\nCLEAN CODE\n' + '='*25 + '\n')
+            for row in clean_code:
                 f.write(row + '\n')
 
-        sg.popup_quick_message('hello')
-        print('\n\nCount of lines:', line_count)
-        print('Count of characters:', sum(char_count))
-        print('Avg characters per line:', sum(char_count)//line_count)
-
+        
         # open file with default text editor
         system('output.txt')
+
 
 if __name__ == '__main__':
     main()
